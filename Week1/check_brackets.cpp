faf42aefbaf6c52b7862f31a0489c6e375aa728d
Nhan0504 <ttn0504ltt@gmail.com>
2021-07-26 21:45:19 +0700

    Pass all test cases

--- Week1/check_brackets.cpp
+++ Week1/check_brackets.cpp
@@ -27,8 +27,8 @@ struct Bracket {
 int main() {
     string text;
     getline(cin, text);
-    int count = 0;
     stack <Bracket> opening_brackets_stack;
+    int count = 1;
     for (int position = 0; position < text.length(); position++) {
         char next = text[position];
 
@@ -37,21 +37,31 @@ int main() {
         }
 
         if (next == ')' || next == ']' || next == '}') {
-            if (opening_brackets_stack.empty() || !opening_brackets_stack.top().Matchc(next)) {
-                count = position + 1;
-                cout << "position = " << position << endl;
-                cout << "count = " << count << endl;
+            if (!opening_brackets_stack.empty()) {
+                if (opening_brackets_stack.top().Matchc(next)) {
+                    opening_brackets_stack.pop();
+                    count = position + 1;
+                }
+                else {
+                    cout << position + 1 << endl;                   
+                    return 0;
+                }
             }
-            opening_brackets_stack.pop();
+            else {
+                cout << position + 1 << endl;
+                return 0;
+            }
+              
         }
     }
 
-    if (count == 0 && opening_brackets_stack.empty()) {
+    if (opening_brackets_stack.empty()) {
         cout << "Success" << endl;
     }
     else {
-        cout << count << endl;
+        cout << opening_brackets_stack.top().position + 1 << endl;
     }
+    
 
     return 0;
 }
